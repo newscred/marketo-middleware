90b1f23da3e6cb3b646c04a5f1e3abac7031bd58
A K M Zahiduzzaman <zhd.zmn@gmail.com>
2024-07-10 14:48:13 -0400

    fix final logger and log level

--- logger.js
+++ logger.js
@@ -10,29 +10,25 @@ const destination =
 const pinoLogger = pino(
   {
     name: process.env.APP_ID,
-    level: process.env.LOG_LEVEL,
+    level: process.env.LOG_LEVEL || 'info',
   },
   destination
 );
 
-/*
-The finalLogger is a special logger instance that will synchronously and reliably flush every log line.
-This is important in exit handlers, since no more asynchronous activity may be scheduled.
-*/
 process.on(
   'uncaughtException',
-  pino.final(pinoLogger, (err, finalLogger) => {
-    finalLogger.error(err, 'uncaughtException');
+  (err) => {
+    pinoLogger.error(err, 'uncaughtException');
     process.exit(1); // eslint-disable-line no-process-exit
-  })
+  }
 );
 
 process.on(
   'unhandledRejection',
-  pino.final(pinoLogger, (err, finalLogger) => {
-    finalLogger.error(err, 'unhandledRejection');
+  (err) => {
+    pinoLogger.error(err, 'unhandledRejection');
     process.exit(1); // eslint-disable-line no-process-exit
-  })
+  }
 );
 
 // Reopen the destination after logrotate does it's magic!
