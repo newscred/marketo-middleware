89a072f911b36fa942ceafc8e1382e6b704a09cb
A K M Zahiduzzaman <zhd.zmn@gmail.com>
2024-08-06 13:33:21 -0400

    move to a ISO datestring stamp for published content

--- marketo/index.js
+++ marketo/index.js
@@ -74,7 +74,7 @@ export async function publishMarketo(req, res) {
   await marketo.initialize();
 
   // generate marketo email
-  const { previewURL } = await generateEmail(marketo, mktoTokens, `${structuredContent.title}-${Date.now()}`);
+  const { previewURL } = await generateEmail(marketo, mktoTokens, `${structuredContent.title}-${(new Date()).toISOString()}`);
   appLogger.info({url: previewURL}, 'preview generated');
 
   await postPublicAPI(token, payload.data.publishing_event.links.publishing_metadata, {
